44eecd61b7922e7fa9c8bb2a759bdeb24e42a001
Dmitrii Bychkov <mail@dbychkov.com>
2021-08-12 04:45:12 +0300

    Update dependencies

---
+++ buildSrc/src/main/java/Configuration.kt
@@ -0,0 +1,10 @@
+object Configuration {
+    const val compileSdkVersion = 30
+    const val targetSdkVersion = 29
+    const val minSdkVersion = 21
+
+    const val versionCode = 1
+    const val versionName = "1.0.0"
+
+    const val applicationId = "com.dbychkov.spacex"
+}
--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -1,39 +1,53 @@
 object Libs {
 
     object Kotlin {
-        private const val version = "1.5.10"
-        const val stdlib = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:$version"
-        const val gradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:$version"
-        const val extensions = "org.jetbrains.kotlin:kotlin-android-extensions:$version"
+        const val stdlib = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:${Versions.KOTLIN}"
+        const val gradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.KOTLIN}"
+        const val extensions = "org.jetbrains.kotlin:kotlin-android-extensions:${Versions.KOTLIN}"
     }
 
     object Coroutines {
-        private const val version = "1.5.1"
-        const val core = "org.jetbrains.kotlinx:kotlinx-coroutines-core:$version"
-        const val android = "org.jetbrains.kotlinx:kotlinx-coroutines-android:$version"
-        const val test = "org.jetbrains.kotlinx:kotlinx-coroutines-test:$version"
+        const val core = "org.jetbrains.kotlinx:kotlinx-coroutines-core:${Versions.COROUTINES}"
+        const val android = "org.jetbrains.kotlinx:kotlinx-coroutines-android:${Versions.COROUTINES}"
+        const val test = "org.jetbrains.kotlinx:kotlinx-coroutines-test:${Versions.COROUTINES}"
     }
-
 
     object Network {
         object Retrofit {
-            private const val version = "2.9.0"
-            const val retrofit = "com.squareup.retrofit2:retrofit:$version"
-            const val moshiConverter = "com.squareup.retrofit2:converter-moshi:$version"
+            const val retrofit = "com.squareup.retrofit2:retrofit:${Versions.RETROFIT}"
+            const val moshiConverter = "com.squareup.retrofit2:converter-moshi:${Versions.RETROFIT}"
         }
 
         object OkHttp {
-            private const val version = "4.8.0"
-            const val okhttp = "com.squareup.okhttp3:okhttp:$version"
-            const val loggingInterceptor = "com.squareup.okhttp3:logging-interceptor:$version"
+            const val okhttp = "com.squareup.okhttp3:okhttp:${Versions.OKHTTP}"
+            const val loggingInterceptor = "com.squareup.okhttp3:logging-interceptor:${Versions.OKHTTP}"
+            const val mockWebserver = "com.squareup.okhttp3:mockwebserver:${Versions.OKHTTP}"
         }
 
         object Moshi {
-            private const val version = "1.12.0"
-            const val moshi = "com.squareup.moshi:moshi:$version"
-            const val codegen = "com.squareup.moshi:moshi-kotlin-codegen:$version"
-            const val adapters = "com.squareup.moshi:moshi-adapters:$version"
+            const val moshi = "com.squareup.moshi:moshi:${Versions.MOSHI}"
+            const val codegen = "com.squareup.moshi:moshi-kotlin-codegen:${Versions.MOSHI}"
+            const val adapters = "com.squareup.moshi:moshi-adapters:${Versions.MOSHI}"
         }
+    }
+
+    object Coil {
+        private const val version = "1.3.0"
+        const val composeCoil = "io.coil-kt:coil-compose:$version"
+    }
+
+    object Compose {
+        const val composeCompiler = "androidx.compose.compiler:compiler:${Versions.COMPOSE}"
+        const val composeUi = "androidx.compose.ui:ui:${Versions.COMPOSE}"
+        const val composeUiTooling = "androidx.compose.ui:ui-tooling:${Versions.COMPOSE}"
+        const val composeFoundation = "androidx.compose.foundation:foundation:${Versions.COMPOSE}"
+        const val composeMaterial = "androidx.compose.material:material:${Versions.COMPOSE}"
+        const val composeMaterialIconsCore = "androidx.compose.material:material-icons-core:${Versions.COMPOSE}"
+        const val composeMaterialIconsExtended = "androidx.compose.material:material-icons-extended:${Versions.COMPOSE}"
+        const val composeAccompanistPermissions = "com.google.accompanist:accompanist-permissions:${Versions.COMPOSE_ACCOMPANIST}"
+        const val composeAccompanistPager = "com.google.accompanist:accompanist-pager:${Versions.COMPOSE_ACCOMPANIST}"
+        const val composeActivity = "androidx.activity:activity-compose:${Versions.COMPOSE_ACTIVITY}"
+        const val composeViewModel = "androidx.lifecycle:lifecycle-viewmodel-compose:${Versions.COMPOSE_VIEW_MODEL}"
     }
 }
 
